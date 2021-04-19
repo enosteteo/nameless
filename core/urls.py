f6afc6ec3ee4dab4498163422b699495cd3399f1
Enos Tetéo <enosfrancisco@gmail.com>
2021-04-19 17:58:26 -0300

    tutorial 3, 4 e parte do 5 django

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = 'core'
 urlpatterns = [
-    path('', views.index, name='index'),
+    # ex: /core/
+    path('', views.IndexView.as_view(), name='index'),
+    # ex: /core/5/
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # ex: /core/5/results/
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # ex: /core/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote')
 ]
